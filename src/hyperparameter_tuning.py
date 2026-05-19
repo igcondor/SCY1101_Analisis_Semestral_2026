391a74109f4f88d5c4c7ba45c2140553faf4bb31
Paltaxd2 <raf.albornoz@duocuc.cl>
2026-05-19 02:47:29 +0000

    Actualizando hyperparameter en src

--- src/hyperparameter_tuning.py
+++ src/hyperparameter_tuning.py
@@ -8,70 +8,57 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
+import sys
+sys.path.append('.')
+from src.preprocesamiento_data import pipeline_clasificacion
+
 def cargaData():
-  df_pipeline = pd.read_csv("../clean_retail_store_sales.csv")
+  df_pipeline = pd.read_csv("clean_retail_store_sales.csv")
   df_auto = df_pipeline.copy()
   return df_auto
 
-def definirxy(df_auto, columnaBuscada: str):
-  y = df_auto[columnaBuscada]
-  X = df_auto.drop(columns=['Transaction ID',	'Customer ID',
-                            'Transaction Date', 'Timestamp', columnaBuscada])
-  
+def definirxy(df_auto):
+  X, y = pipeline_clasificacion(df_auto)
+
+  # División: 80% entrenamiento, 20% prueba
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
   return X_train, X_test, y_train, y_test
 
-def preprocesador(df_auto, columnaBuscada: str):
-  df_auto = df_auto.drop(columns=['Transaction ID',	'Customer ID', 'Transaction Date', 
-                                  'Timestamp', columnaBuscada]).copy()
-  cateList = df_auto.select_dtypes(include=["object"]).columns.to_list()
-  numList = df_auto.select_dtypes(include=["int64", "float64", "bool"]).columns.to_list()
+def optimizacion(X_train, y_train):
+  modelo = RandomForestClassifier(random_state=42)
 
-  preprocessor = ColumnTransformer(transformers=[
-  ('cat', OneHotEncoder(), cateList),
-  ('num', StandardScaler(), numList)
-  ])
-  return preprocessor
-
-def pipeline(preprocessor):
-  full_pipeline = Pipeline(steps=[
-      ('preprocessor', preprocessor),
-      ('classifier', RandomForestClassifier(random_state=42))
-  ])
-  return full_pipeline
-
-def entrenamiento(full_pipeline, X_train, y_train):
   param_grid = {
-      'classifier__n_estimators': [10, 50, 100],      # Cantidad de árboles
-      'classifier__max_depth': [None, 3, 5, 10],      # Profundidad de los árboles
-      'classifier__criterion': ['gini', 'entropy']    # Medida de calidad de división
-  }
+      'n_estimators': [10, 50, 100],      # Cantidad de árboles
+      'max_depth': [None, 3, 5, 10],      # Profundidad de los árboles
+      'criterion': ['gini', 'entropy']    # Medida de calidad de división
+    }
 
   grid_search = GridSearchCV(
-        estimator=full_pipeline,
+        estimator=modelo,
         param_grid=param_grid,
         cv=5,
         scoring='accuracy',
-        # n_jobs=-1,    # SOLO USAR EN COLAB, esto te usa todos los nucleos para calcular
+        n_jobs=-1,          # SOLO USAR EN COLAB, esto te usa todos los nucleos para calcular
         verbose=1
-      )
+    )
+
+  # Ejecutar la optimización
   grid_search.fit(X_train, y_train)
   return grid_search
 
 def resultados(grid_search, X_test, y_test):
   print(f"Mejores Hiperparámetros: {grid_search.best_params_}")
   print(f"Mejor Precisión (Accuracy) en entrenamiento: {grid_search.best_score_:.4f}")
+
   y_pred = grid_search.predict(X_test)
   print("\nReporte de Clasificación Final:")
   print(classification_report(y_test, y_pred))
 
-def ejecucion(columnaBuscada: str):
+def ejecucion():
   df_auto = cargaData()
-  X_train, X_test, y_train, y_test = definirxy(df_auto, columnaBuscada)
-  preprocessor = preprocesador(df_auto, columnaBuscada)
-  full_pipeline = pipeline(preprocessor)
-  grid_search = entrenamiento(full_pipeline, X_train, y_train)
+  X_train, X_test, y_train, y_test = definirxy(df_auto)
+  grid_search = optimizacion(X_train, y_train)
   resultados(grid_search, X_test, y_test)
-  return resultados
+  return "Ejecucion de Hiperparametro, terminada"
 
-# ejecucion("Category")
+# ejecucion()
